de99f98efa36ec055759d38778e943cc07024312
Rafael Isotton <rafaisotton475@gmail.com>
2026-09-11 00:25:36 -0300

    Add files via upload

--- calculadora/main.py
+++ calculadora/main.py
@@ -52,15 +52,19 @@ def submit_form():
     # Declarar variáveis para a coleta dos dados
     name = request.form['name']
     email = request.form['email']
-    adress = request.form['address']
+    address = request.form['address']
     date = request.form['date']
-    
-    # Aqui você pode salvar os dados ou enviá-los por email
+
+    with open('form.txt', 'a', encoding='utf-8') as file:
+        file.write(name + '\n')
+        file.write(email + '\n')
+        file.write(address + '\n')
+        file.write(date + '\n')
+
     return render_template('form_result.html', 
-                           # Coloque as variáveis aqui
                            name=name,
                            email=email,
-                           address=adress,
+                           address=address,
                            date=date
                            )
 
